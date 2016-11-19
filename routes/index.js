88b29a80f2f26bda68f9609740369b3e4580f912
PLDaily <pcd12321@gmail.com>
2016-11-19 12:01:10 +0800

    密码使用md5加密

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var User = require('../models/user.js');
+var crypto = require('crypto');
 var router = express.Router();
 
 /* GET home page. */
@@ -18,6 +19,8 @@ router.post('/reg', function(req, res, next) {
 	var password_repeat = req.body['password-repeat'];
 	var email = req.body.email;
 
+	var md5 = crypto.createHash('md5');
+	password = md5.update(password).digest('hex');
 	var user = {
 		name: name,
 		password: password,
@@ -55,6 +58,9 @@ router.post('/login', function(req, res, next) {
 	console.log(req.body.password);
 	var name = req.body.name;
 	var password = req.body.password;
+	
+	var md5 = crypto.createHash('md5');
+	password = md5.update(password).digest('hex');
 	
 	User.get(name, function(err, user) {
 		if(!user) {
